c75d881ae2c7df0c5feb42182354bfeadfe7286f
spinolawill <36300773+spinolawill@users.noreply.github.com>
2019-05-03 18:40:18 -0400

    Plotados os mockups no mapa; Criada a lista dos locais; Iniciado a parte de links uteis

--- scripts/locals.js
+++ scripts/locals.js
@@ -2,7 +2,42 @@ var map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+        center: { lat: -22.227520, lng: -54.809613 },
+        zoom: 14
     });
+    
+    var card_list = document.querySelector("#card-columns");
+
+    for (var i = 0; i < locals_list.length; i++) {
+    	console.log("ola mundo")
+    	new google.maps.Marker ({
+    		position: {
+    			lat: locals_list[i].lat,
+    			lng: locals_list[i].lng
+    		},
+
+    		map: map,
+    		title: locals_list[i].name
+    	})
+
+
+    	card_list.innerHTML +=
+    		`<div class="card mb-3 mr-5 col-sm-12 col-md-6 col-lg-4">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="${locals_list[i].img}" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">${locals_list[i].name}</h5>
+                    <p class="card-text">${locals_list[i].category}</p>
+                    <p class="card-text badge badge-success">${locals_list[i].option_type}</p>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+   		
+    }
 }
+
+
---
+++ scripts/mockup-locals.js
@@ -0,0 +1,287 @@
+var locals_list =  [
+ {
+    "lat": -22.228341,
+    "lng": -54.817493,
+    "name": "Cantina do Paulão",
+    "img": "https://scontent.fmgf1-2.fna.fbcdn.net/v/t1.0-9/49479731_1939677809460923_5090699692270944256_n.png?_nc_cat=110&_nc_ht=scontent.fmgf1-2.fna&oh=44d8c2f197d605b88b23487c916e0674&oe=5D6C474D",
+    "option_type": "Possui opção vegana",
+    "category": "Hamburgueria · Bar · Restaurante",
+    "address": "Av. Marcelino Píres, 1187 - Centro, Dourados - MS, 79820-010",
+    "phone": "(67) 3421-8238",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https//www.facebook.com/cantinadopaulao2/"
+      }
+    ],
+    "modality": "la carte, KG, Livre",
+    "products": [
+      {
+        "name": "Vegano: lanche vegano com hambúrguer artesanal de grão de bico, cenoura e aveia, salada de alface, rúcula, picles, tomate e pasta de grão de bico homus."
+      },
+      {
+        "name": "Macarrão vegano bolonhesa: proteína de soja + massa sem ovos"
+      }
+    ]
+  },
+  {
+    "lat": -22.225637,
+    "lng": -54.809675,
+    "name": "Baru Mercado Natural",
+    "img": "https://scontent.fmgf1-2.fna.fbcdn.net/v/t1.0-9/30739931_583357632028368_8747150534135250944_n.png?_nc_cat=100&_nc_ht=scontent.fmgf1-2.fna&oh=2d0b7968cd14f2b5f934357e7b230989&oe=5D653C14",    
+    "option_type": "Possui opção vegana",
+    "category": "Especializado",
+    "address": "Rua Doutor Nelson de Araújo, 737 - Jardim América, Dourados - MS, 79804-040",
+    "phone": "(67)3038-8006",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/BaruMercadoNatural"
+      },
+      {
+        "type": "Insta",
+        "link": "https://www.instagram.com/barumercadonatural/"
+      }
+    ],
+    "modality": "la carte, KG, Livre",
+    "products": [
+      {
+        "name": "Loja de produtos naturais: são mais de 250 produtos à granel, com várias opções de castanhas, temperos, chás, farinhas, além de muitos produtos light, diet, sem lactose, sem glúten, veganos e muito mais !"
+      }
+    ]
+  },
+  {
+    "lat": -22.225784,
+    "lng": -54.806803,
+    "name": "The 49",
+    "img": "https://scontent.fmgf1-2.fna.fbcdn.net/v/t1.0-9/49209838_2197495907184463_4345552385725169664_n.png?_nc_cat=111&_nc_ht=scontent.fmgf1-2.fna&oh=0aab8fb29c06c98d77cae115dadea63a&oe=5D6D281B",
+    "option_type": "Possui opção vegana",
+    "category": "Chopperia · Restaurante",
+    "address": "Rua Dr. Camilo Hermelindo da Silva, 710 - Jardim Central, Dourados - MS, 79826-070",
+    "phone": "(67) 3423-0332",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/the49pub/"
+      },
+      {
+        "type": "Insta",
+        "link": "https://www.instagram.com/the49cafe/"
+      }
+    ],
+    "modality": "la carte, KG , Livre",
+    "products": [
+      {
+        "name": "San Francisco soup: caldo vegano de legumes",
+        "type": "Vegano"
+      },
+      {
+        "name": "Vegan Salad: alface, rúcula, tomate, picles, azeitona, queijo fresco e croutons. 4 molhos a escolha: azeite de ervas, molho de iogurte, molho de pimenta e molho balsâmico. Todos artesanais",
+        "type": "Vegano"
+      },
+      {
+        "name": "Pirita Vegan: lanche vegano com hambúrguer artesanal de grão de bico ou beterraba com lentilha",
+        "type": "Vegano"
+      }
+    ]
+  },
+  {
+    "lat": -22.21598,
+    "lng": -54.835396,
+    "name": "Flor de AMORa Bistrô",
+    "img": "https://scontent.fmgf1-1.fna.fbcdn.net/v/t1.0-9/38085839_319675085244466_7407626272613859328_n.png?_nc_cat=104&_nc_ht=scontent.fmgf1-1.fna&oh=83fa3617fe695a5c3dd9225fb6f66d40&oe=5D70483E",
+    "option_type": "Possui opção vegana",
+    "category": "Restaurante",
+    "address": "Rua Reinaldo Bianchi, 398 - Parque Alvorada, Dourados - MS, 79823-381",
+    "phone": "(67) 98416-6685",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/oi.baianinha/"
+      },
+      {
+        "type": "Insta",
+        "link": "https://www.instagram.com/oi.baianinha/"
+      }
+    ],
+    "modality": "la carte",
+    "products": [
+      {
+        "name": "Tortas, Bolos, Cupcakes, Muffins, Cookies, Docinhos, Salgadinhos, Opções sem glúten",
+        "type": "Vegano"
+      }
+    ]
+  },
+  {
+    "lat": -22.24892,
+    "lng": -54.805136,
+    "name": "Restaurante Vegetariano Natural",
+    "img": "https://scontent.fmgf1-2.fna.fbcdn.net/v/t1.0-9/13244845_699712846835698_7322501885913022525_n.jpg?_nc_cat=102&_nc_ht=scontent.fmgf1-2.fna&oh=434733aaf83046ba1295d68220587913&oe=5D2F43A0",
+    "option_type": "Possui opção vegana",
+    "category": "Restaurante",
+    "address": "R. Hayel Bon Faker, 500 - Jardim Água Boa, Dourados - MS, 79813-240",
+    "phone": "(67) 3425-9342",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/restveg2004/"
+      }
+    ],
+    "modality": "KG, Livre",
+    "products": [
+      {
+        "name": "Comidas orientais deliciosas & saudáveis!"
+      }
+    ]
+  },
+  {
+    "lat": -22.220194,
+    "lng": -54.824767,
+    "name": "Santo Canto",
+    "img": "https://scontent.fmgf1-2.fna.fbcdn.net/v/t1.0-9/48407713_2223660524615059_5264894554955317248_n.png?_nc_cat=111&_nc_ht=scontent.fmgf1-2.fna&oh=4b5a17ef648d62c4c3f07e3949498745&oe=5D73237B",
+    "option_type": "Possui opção vegetariana",
+    "category": "Hamburgueria",
+    "address": "R. Ponta Porã, 705 - Jardim America, Dourados - MS, 79824-130",
+    "phone": "(67) 3427-4725 | (67) 99896-4878",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/santocantodourados"
+      },
+      {
+        "type": "Insta",
+        "link": "https://www.instagram.com/santo_canto/"
+      }
+    ],
+    "modality": "la carte",
+    "products": [
+      {
+        "name": "Veggie: pão integral, alface americana, tomate, cebola roxa e hambúrguer de soja",
+        "type": "Vegetariano"
+      }
+    ]
+  },
+  {
+    "lat": -22.214231,
+    "lng": -54.811169,
+    "name": "Michelito",
+    "img": "https://scontent.fmgf1-2.fna.fbcdn.net/v/t1.0-9/22365182_1264154343691166_6425808962490370546_n.jpg?_nc_cat=102&_nc_ht=scontent.fmgf1-2.fna&oh=10002f124bf78db062e4c788fd8d71fc&oe=5D6190B8",
+    "option_type": "Possui opção vegetariana",
+    "category": "Hamburgueria · Lanchonete · Restaurante fast-food",
+    "address": "R. Antônio de Carvalho, 1426-1490 - Vila Planalto, Dourados - MS, 79826-030",
+    "phone": "(67) 3422-2806",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/michelitolanches/"
+      },
+      {
+        "type": "Insta, ",
+        "link": "https://www.instagram.com/michelito.hamburgueria/"
+      }
+    ],
+    "modality": "la carte",
+    "products": [
+      {
+        "name": "Vegetariano: Hamburguer de Grão de Bico, Maionese, Queijo, Rúcula, Alface, e Tomate",
+        "type": "Vegetariano"
+      }
+    ]
+  },
+  {
+    "lat": -22.236392,
+    "lng": -54.821164,
+    "name": "Tabasco Burguer",
+    "img": "https://scontent.fmgf1-1.fna.fbcdn.net/v/t1.0-9/53145599_2101168130122426_2810282094880620544_n.jpg?_nc_cat=108&_nc_ht=scontent.fmgf1-1.fna&oh=588a5bb737952fe6251aebf9f9387f06&oe=5D2ED2E0",
+    "option_type": "Possui opção vegetariana",
+    "category": "Hamburgueria · Restaurante fast-food",
+    "address": "R. Gen. Osório, 1111 - Jardim Independência, Dourados - MS, 79814-400",
+    "phone": "(67) 3423-6426",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/TabascoBurger/"
+      }
+    ],
+    "modality": "la carte",
+    "products": [
+      {
+        "name": "Ramones: hambúrguer de soja, queijo mussarela e queijo prato",
+        "type": "Vegetariano"
+      }
+    ]
+  },
+  {
+    "lat": -22.239634,
+    "lng": -54.799789,
+    "name": "Urbanos Hot Dog",
+    "img": "https://scontent.fmgf1-1.fna.fbcdn.net/v/t1.0-9/53154973_2032442047056924_8489952374733930496_n.jpg?_nc_cat=105&_nc_ht=scontent.fmgf1-1.fna&oh=72c96c7602236b6d979101dac6a6beee&oe=5D2EF7AD",
+    "option_type": "Possui opção vegetariana",
+    "category": "Lanchonete",
+    "address": "R. João Damaceno Pires, 1604 - Jardim Água Boa, Dourados - MS, 79811-070",
+    "phone": "(67) 99690-8080",
+    "social_networks": [
+      {
+        "type": "Site, FB, TT, Insta, ",
+        "link": "https://www.facebook.com/urbanoshotdog"
+      }
+    ],
+    "modality": "la carte, KG, Livre",
+    "products": [
+      {
+        "name": "Bino: maionese temperada, rúcula, alface, cebola roxa, tomate, milho, ervilha, queijo, catupiry e batata palha",
+        "type": "Vegetariano"
+      }
+    ]
+  },
+  {
+    "lat": -22.22271,
+    "lng": -54.824253,
+    "name": "Be Blessed Gourmet",
+    "img": "https://scontent.fmgf1-2.fna.fbcdn.net/v/t1.0-9/50407315_2185765028419032_452591112347975680_n.png?_nc_cat=110&_nc_ht=scontent.fmgf1-2.fna&oh=4d225223a576a34ffaeac7a0bfd26f44&oe=5D6A18E7",
+    "option_type": "Possui opção vegana",
+    "category": "Hamburgueria · Lanchonete · Restaurante fast-food",
+    "address": "R. Pedro Celestino, 1239 - Jardim Tropical, Dourados - MS, 79803-070",
+    "phone": "(67) 3037-1467",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/beblessedgourmet/"
+      },
+      {
+        "type": "Insta",
+        "link": "https://www.instagram.com/bblessed.gourmet/"
+      }
+    ],
+    "modality": "la carte, KG, Livre",
+    "products": [
+      {
+        "name": "Be Vegetarian/Be Vegan: pão australiano, alface, tomate, cebola roxa, hambúrguer de soja, mussarela, rúcula crocante e fritas",
+        "type": "Vegano"
+      }
+    ]
+  },
+  {
+    "lat": -22.221958,
+    "lng": -54.820041,
+    "option_type": "Possui opção vegetariana",
+    "name": "NaKaranga FoodTrailer",
+    "img": "https://scontent.fmgf1-1.fna.fbcdn.net/v/t1.0-9/14680732_316738642009719_7018679532886045744_n.png?_nc_cat=105&_nc_ht=scontent.fmgf1-1.fna&oh=cd702d0ef2b22f379b7a70bb2ef6dee8&oe=5D297079",
+    "category": "Hamburgueria · Bar",
+    "address": "R. Benjamin Constant, 1214 - BNH I Plano, Dourados - MS, 79824-120",
+    "phone": "(67) 98186-3282 // 99650-9617",
+    "social_networks": [
+      {
+        "type": "FB",
+        "link": "https://www.facebook.com/nakaranga/"
+      }
+    ],
+    "modality": "la carte",
+    "products": [
+      {
+        "name": "Vegetariano burger: rúcula, cream cheese, tomate seco, molho mac, palmito, cebola roxa, maionese, queijo prato",
+        "type": "Vegetariano"
+      }
+    ]
+  }
+]
